0b8a5148cbf7ba58e05c8a107cfa20928cf2a071
Edward Williams <eddiecwilliams@gmail.com>
2023-02-19 21:24:42 -0600

    no more scripts

--- scripts/public_data/crop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=/local/ecw/DeepScribe_Data_2023-02-04-selected/public
-
-python deepscribe2/preprocessing/crop_images.py --json $DATA_DIR/imagesWithHotspots_public.json \
-                                                --raw_imgs $DATA_DIR/images_public \
-                                                --cropped_imgs $DATA_DIR/cropped_images_public \
-                                                --cropped_json $DATA_DIR/imagesWithHotspots_cropped_public.json
--- scripts/public_data/make_splits.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=/local/ecw/DeepScribe_Data_2023-02-04-selected/public
-
-python deepscribe2/preprocessing/split_tablet.py --json $DATA_DIR/imagesWithHotspots_cropped_public.json \
-                                                                        --splits 0.8 0.1 0.1 \
-                                                                        --fold_suffixes train val test \
-                                                                        --prefix $DATA_DIR/data_public
